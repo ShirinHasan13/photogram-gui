226ee660ab726a4da19ab4df1bbbf9bc231a73aa
ShirinHasan13 <shirin10@chicagobooth.edu>
2024-07-24 01:38:58 +0000

    d

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -38,7 +38,28 @@ def create
  a_new_photo.save
 
 #render({:template => "photo_templates/create"})
+next_url = "/photos/" + a_new_photo.id.to_s
+redirect_to(next_url)
+end
 
-redirect_to("/photos/" + a_new_photo.id.to_s)
+def update 
+  #Parameters: {"query_image"=>"https://www.chicagobooth.edu/-/media/project/chicago-booth/why-booth/a-global-footprint/building-connections-across-asia/chicago-booth-hong-kong-location.jpg?cx=0.39&cy=0.55&cw=940&ch=749&hash=FE37658076B8C0C5FED3540E489CAE91", "query_caption"=>"Chicago Booth updated", "modify_id"=>"951"}
+
+  the_id = params.fetch("modify_id")
+
+  matching_photos = Photo.where({ :id => the_id })
+
+ the_photo = matching_photos.at(0)
+
+ input_image = params.fetch("query_image")
+ input_caption = params.fetch("query_caption")
+ 
+ the_photo.image = input_image
+ the_photo.caption = input_caption
+ the_photo.save
+
+  #render({:template => "photo_templates/update"})
+ next_url =  "/photos/" + the_photo.id.to_s
+redirect_to(next_url)
 end
 end
